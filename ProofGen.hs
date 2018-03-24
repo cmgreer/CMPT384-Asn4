afc7b309782d144f867ff98f664b7a77362348a3
Cole Greer <cole@Coles-MacBook-Pro.local>
2018-03-23 20:25:34 -0700

    restructured proof result, added exports

--- ProofGen.hs
+++ ProofGen.hs
@@ -1,26 +1,33 @@
-module ProofGen (PF (..), ProofConjecture (..), ProofResult (..)) where
+module ProofGen (PF (..), ProofConjecture (..), ProofResult (..),
+  Rule (..), Result (..), wang) where
 
 data PF = Prop Char | Neg PF | Conj PF PF |
           Disj PF PF | Imp PF PF | Equiv PF PF
-          deriving Show
+          deriving (Show, Eq)
 
 data ProofConjecture = Conjecture [PF] [PF]
                        deriving Show
 
 --data ProofResult = proven/refuted, Rule num, Sub-proofs/refutation
-data ProofResult = Proven Rule [ProofResult] |
-                   Refuted Rule ProofResult
+data ProofResult = NoSub Rule Result ProofConjecture |
+                   OneSub Rule Result ProofResult |
+                   TwoSub Rule Result ProofResult ProofResult
+
 type Rule = [Char]
+
+data Result = Proven | Refuted
+
 
 --Format rules: Prop's should come at start of list
 wang :: ProofConjecture -> ProofResult
 
 wang (Conjecture ((Prop p):h) g) = case rule1 (Conjecture ((Prop p):h) g) of
   (Just result) -> result
-  --_
+  _ -> wang ((Conjecture (h++(Prop p):[]) g))
 
 --Temp code unfinished
 rule1 :: ProofConjecture -> Maybe ProofResult
 
-rule1 (Conjecture ((Prop p):h) g) = Nothing
--- | elem (Prop p) g = 
+rule1 (Conjecture ((Prop p):h) g)
+ | elem (Prop p) g = Just (NoSub "1a" Proven (Conjecture ((Prop p):h) g))
+ | otherwise = rule1 (Conjecture h g)
