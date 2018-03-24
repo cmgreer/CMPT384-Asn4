e2c70da5ea76f0ce517f64e42f3dae29e887ead7
Cole Greer <cole@Coles-MacBook-Pro.local>
2018-03-23 21:22:21 -0700

    modified proofResult definition

--- ProofGen.hs
+++ ProofGen.hs
@@ -8,9 +8,7 @@ data PF = Prop Char | Neg PF | Conj PF PF |
 data ProofConjecture = Conjecture [PF] [PF]
                        deriving Show
 
-data ProofResult = NoSub Rule Result ProofConjecture |
-                   OneSub Rule Result ProofResult |
-                   TwoSub Rule Result ProofResult ProofResult
+data ProofResult = Proof Rule Result ProofConjecture [ProofResult]
 
 type Rule = [Char]
 
@@ -18,19 +16,35 @@ data Result = Proven | Refuted
 
 wang :: ProofConjecture -> ProofResult
 
+wang (Conjecture ((Prop ph):h) ((Prop pg):g)) = case rule1 (Conjecture ((Prop ph):h) ((Prop pg):g)) of
+  (Just result) -> result
+  _ -> wang (Conjecture (h++(Prop ph):[]) (g++(Prop pg):[]))
+
+--oneSubRules
+wang (Conjecture ((Neg p):h) g) = case rule1 (Conjecture ((Neg p):h) g) of
+  (Just result) -> result
+  _ -> case wang (Conjecture h (p:g)) of
+    (Proof rule result conjecture subproofs) ->
+      (Proof "2a" result (Conjecture ((Neg p):h) g) [(Proof rule result conjecture subproofs)])
+
 wang (Conjecture ((Prop p):h) g) = case rule1 (Conjecture ((Prop p):h) g) of
   (Just result) -> result
-  _ -> wang ((Conjecture (h++(Prop p):[]) g))
+  _ -> wang (Conjecture (h++(Prop p):[]) g)
+wang (Conjecture h ((Prop p):g)) = case rule1 (Conjecture h ((Prop p):g)) of
+  (Just result) -> result
+  _ -> wang (Conjecture h (g++(Prop p):[]))
+
+--TwoSubRules
 
 rule1 :: ProofConjecture -> Maybe ProofResult
-rule1 (Conjecture [] []) = Just (NoSub "1b" Refuted (Conjecture [] []))
-rule1 (Conjecture h []) = Nothing
-rule1 (Conjecture [] g) = Nothing
+rule1 (Conjecture [] []) = Just (Proof "1b" Refuted (Conjecture [] []) [])
+rule1 (Conjecture h []) = Just (Proof "1b" Refuted (Conjecture [] []) [])
+rule1 (Conjecture [] g) = Just (Proof "1b" Refuted (Conjecture [] []) [])
 rule1 (Conjecture ((Prop p):h) g)
- | elem (Prop p) g = Just (NoSub "1a" Proven (Conjecture ((Prop p):h) g))
+ | elem (Prop p) g = Just (Proof "1a" Proven (Conjecture ((Prop p):h) g) [])
  | otherwise = case rule1 (Conjecture h g) of
-   Just (NoSub rule result (Conjecture hs gs)) -> Just (NoSub rule result (Conjecture (hs++(Prop p):[]) gs))
+   Just (Proof rule result (Conjecture hs gs) prs) -> Just (Proof rule result (Conjecture (hs++(Prop p):[]) gs) prs)
    result -> result
 rule1 (Conjecture (p:h) g) = case rule1 (Conjecture h g) of
-    Just (NoSub rule result (Conjecture hs gs)) -> Just (NoSub rule result (Conjecture (hs++p:[]) gs))
+    Just (Proof rule result (Conjecture hs gs) prs) -> Just (Proof rule result (Conjecture (hs++p:[]) gs) prs)
     result -> result
