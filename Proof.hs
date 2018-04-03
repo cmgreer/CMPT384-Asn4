0fe28460bd655b5156099e111f1877bf05438306
Avery Crespi <acrespi@sfu.ca>
2018-04-03 00:16:48 +0000

    Add reduction information to show

--- Proof.hs
+++ Proof.hs
@@ -1,6 +1,8 @@
 module Proof (PF (..), ProofConjecture (..), ProofResult (..),
   Rule (..), Result (..)) where
 
+import Data.List
+
 data PF = Prop Char | Neg PF | Conj PF PF |
           Disj PF PF | Imp PF PF | Equiv PF PF
           deriving Eq
@@ -42,20 +44,20 @@ printPR (Proof rule Proven (Conjecture h g) []) prefix indent =
     " by rule " ++ rule ++
     " with common proposition " ++ show (head (commonElems h g))
 
--- TODO: add hypothesis or goal formula being reduced
 -- Rules 2a, 2b, 3a, 4b, 5b
 printPR (Proof rule Proven conj [sub]) prefix indent =
     prefix ++ indent ++
     "Proved conjecture " ++ show conj ++
-    " by rule " ++ rule ++
+    " with rule " ++ rule ++
+    " by reducing " ++ show (head (findReduced conj sub)) ++
     " because" ++ (printPR sub "\n" ('\t':indent))
 
--- TODO: add hypothesis or goal formula being reduced
 -- Rules 3b, 4a, 5a, 6a, 6b
 printPR (Proof rule Proven conj [sub1, sub2]) prefix indent =
     prefix ++ indent ++
     "Proved conjecture " ++ show conj ++
-    " by rule " ++ rule ++
+    " with rule " ++ rule ++
+    " by reducing " ++ show (head (findReduced conj sub1)) ++
     " because " ++
     (printPR sub1 "\n" ('\t':indent)) ++
     (printPR sub2 "\n" ('\t':indent))
@@ -65,12 +67,12 @@ printPR (Proof "1b" Refuted conj []) prefix indent =
     prefix ++ indent ++
     "Refuted conjecture " ++ show conj ++ " by rule 1b"
 
--- TODO: add hypothesis or goal formula being reduced
 -- Subrefutation
 printPR (Proof rule Refuted conj subs) prefix indent =
     prefix ++ indent ++
     "Refuted conjecture " ++ show conj ++
-    " by rule " ++ rule ++
+    " with rule " ++ rule ++
+    " by reducing " ++ show (head (findReduced conj (head subs))) ++
     " because " ++ (printPR (head (findRefuted subs)) "\n" ('\t':indent))
 
 printPR _ _ _ = "Print error: unrecognized result format"
@@ -81,9 +83,29 @@ commonElems :: [PF] -> [PF] -> [PF]
 
 commonElems [] _ = []
 commonElems _ [] = []
-commonElems (p:more) other = case elem p other of
-    True -> p : (commonElems more other)
-    _ -> commonElems more other
+commonElems (p:more) second = case elem p second of
+    True -> p : (commonElems more second)
+    _ -> commonElems more second
+
+-- Find PFs that are in the first list but not the second.
+
+onlyFirst :: [PF] -> [PF] -> [PF] -> [PF]
+
+onlyFirst [] _ accum = accum
+onlyFirst first [] accum = first
+onlyFirst (p:more) second accum = case elem p second of
+    True -> onlyFirst more (delete p second) accum
+    _ -> onlyFirst more second (p:accum)
+
+-- Find PF(s) that were reduced.
+
+findReduced :: ProofConjecture -> ProofResult -> [PF]
+
+findReduced (Conjecture h1 g1) (Proof _ _ (Conjecture h2 g2) _) = case onlyFirst h1 h2 [] of
+    (p:more) -> (p:more)
+    _ -> case onlyFirst g1 g2 [] of
+        (p:more) -> (p:more)
+        _ -> []
 
 -- Find refuted ProofResults.
 
